d21a776433e290fd4586e87690210acbd0dd6e77
KaiqueP1 <kaique.liveira@gmail.com>
2024-12-27 20:17:45 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'sql212.epizy.com';
+    $dbUsername = 'epiz_33356896';
+    $dbPassword = 'QppWKlxgtm';
+    $dbName = 'epiz_33356896_formulario_financer';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+    ?>
